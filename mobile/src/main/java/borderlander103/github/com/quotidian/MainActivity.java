6b2fa00daeabb1e8ab642f52285471447abcc8bc
Borderlander103 <mailbox@brianrichardsononline.com>
2018-09-08 20:06:43 -0700

    Add on onClickListener, etc to make button work
    Still needs some debugging, there is a runtime crash on button press

--- mobile/src/main/java/borderlander103/github/com/quotidian/MainActivity.java
+++ mobile/src/main/java/borderlander103/github/com/quotidian/MainActivity.java
@@ -1,7 +1,10 @@
 package borderlander103.github.com.quotidian;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find Button for quotes_list
+        Button quotes = (Button) findViewById(R.id.quotes);
+
+        //Set OnClick listener for quotes_list button
+        quotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent quotesIntent = new Intent(MainActivity.this, ActivityQuoteList.class);
+                startActivity(quotesIntent);
+            }
+        });
+
     }
 }
