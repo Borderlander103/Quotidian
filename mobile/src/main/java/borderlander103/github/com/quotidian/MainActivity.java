56f9a69e6b37fcb1270e94353a4dfc8aec5a993f
Borderlander103 <mailbox@brianrichardsononline.com>
2018-09-09 18:06:35 -0700

    Add and Verify buttons
    Figured out the button was functioning correctly by adding Toast
    message. The runtime error has something to do with calling the
    activity. Added settings button.

--- mobile/src/main/java/borderlander103/github/com/quotidian/MainActivity.java
+++ mobile/src/main/java/borderlander103/github/com/quotidian/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +15,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Find Button for quotes_list
-        Button quotes = (Button) findViewById(R.id.quotes);
+        Button mQuotesList = findViewById(R.id.quotesList);
 
-        //Set OnClick listener for quotes_list button
-        quotes.setOnClickListener(new View.OnClickListener() {
+        //Set OnClick listener for quotes button
+        mQuotesList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent quotesIntent = new Intent(MainActivity.this, ActivityQuoteList.class);
-                startActivity(quotesIntent);
+                Toast.makeText(MainActivity.this, "It's working!", Toast.LENGTH_SHORT).show();
+//                Intent quotesIntent = new Intent(MainActivity.this, ActivityQuoteList.class);
+//                startActivity(quotesIntent);
+            }
+        });
+
+        //Find Button for settings
+        Button mSettings = findViewById(R.id.settings);
+
+        //Set OnClickListener for settings button
+        mSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Yo!", Toast.LENGTH_LONG).show();
             }
         });
 
