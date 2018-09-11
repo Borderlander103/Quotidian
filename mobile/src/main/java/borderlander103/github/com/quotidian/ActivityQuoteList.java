b25073f196990174b65b06b3498f69a0f6dbc456
Borderlander103 <mailbox@brianrichardsononline.com>
2018-09-11 10:29:59 -0700

    Add back button to actionbar
    Added the back option to the two activities.

--- mobile/src/main/java/borderlander103/github/com/quotidian/ActivityQuoteList.java
+++ mobile/src/main/java/borderlander103/github/com/quotidian/ActivityQuoteList.java
@@ -2,7 +2,10 @@ package borderlander103.github.com.quotidian;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +17,22 @@ public class ActivityQuoteList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_activity_quote_list);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
--- mobile/src/main/java/borderlander103/github/com/quotidian/ActivitySettings.java
+++ mobile/src/main/java/borderlander103/github/com/quotidian/ActivitySettings.java
@@ -1,7 +1,10 @@
 package borderlander103.github.com.quotidian;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ActivitySettings extends AppCompatActivity{
 
@@ -10,7 +13,22 @@ public class ActivitySettings extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_activity_settings);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
