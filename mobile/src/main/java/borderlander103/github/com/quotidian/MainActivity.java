836e32f21b72f36bd5165aff4451ff6bb67b198e
Borderlander103 <mailbox@brianrichardsononline.com>
2018-09-14 21:33:53 -0700

    Add help icon (not yet working)

--- mobile/src/main/java/borderlander103/github/com/quotidian/MainActivity.java
+++ mobile/src/main/java/borderlander103/github/com/quotidian/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Toast;
@@ -44,6 +46,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(settingIntent);
             }
         });
-
     }
+
+//    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_help:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+
 }
